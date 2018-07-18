56ee244b29dba58977679ca6aea3f8a8bfad78fd
kadurodrigues <carlos.rodrigues.silva@outlook.com>
2018-07-18 22:41:22 +0100

    Created structure components: header and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'wb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'wb';
